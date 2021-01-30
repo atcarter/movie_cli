75c595c2a923c1009400cd9816db67aef7126aef
Antonio <atcarter005@gmail.com>
2021-01-30 17:03:09 -0600

    update api and movie files

--- lib/api.rb
+++ lib/api.rb
@@ -1,6 +1,33 @@
+#This class is responsible for grabbing the data for a given movie
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+require_relative '../lib/movie.rb'
+
 class API
+    attr_accessor :title, :url 
 
+    URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6eb6e1aa&t="
 
+    def initialize(title)
+        @title = title
+        @url = URL + title
+    end
+
+    def get_response
+        uri = URI.parse(self.url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response)
+    end
 
 
 end
+
+# api = API.new("the matrix")
+# mov = Movie.new(api.parse_json)
+# puts mov.ratings
--- lib/movie.rb
+++ lib/movie.rb
@@ -1,4 +1,14 @@
+#This class creates and keeps track of movie objects so as not to repeatedly call the API for a
+#previously called movie but will also keep a separate list for movies that the user has bookmarked
+
 class Movie
+
+    def initialize(hash)
+        hash.each do |key, value|
+            self.class.attr_accessor(key.downcase)
+            self.send(("#{key.downcase}="), value)
+        end
+    end
 
 
 end
